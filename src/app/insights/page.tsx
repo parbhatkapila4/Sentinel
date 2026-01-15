ab6392786544939c3de660cc09ea50a2026f1f8c
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2026-01-15 23:29:55 +0530

    Added Insight page with chatInterface component

---
+++ src/app/insights/page.tsx
@@ -0,0 +1,9 @@
+import { ChatInterface } from "@/components/chat-interface";
+
+export default function InsightsPage() {
+  return (
+    <div className="h-screen w-screen overflow-hidden">
+      <ChatInterface />
+    </div>
+  );
+}
